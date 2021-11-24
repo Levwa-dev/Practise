5a2c1d4fd2a9d32b3e286a2d5f7213d3a32bf68d
Levwa-dev <dmitriiev.valieii65@gmail.com>
2021-11-24 09:04:45 +0200

    practise

---
+++ js/slick.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.team__slider').slick({
+        dots:true,
+        arrows: false
+});
+});
+$(document).ready(function(){
+    $('.test__slider').slick({
+        dots:true,
+        arrows: false
+    });
+});
+$(document).ready(function(){
+    $('.news__slider').slick({
+        dots:true,
+        arrows: false
+    });
+});
